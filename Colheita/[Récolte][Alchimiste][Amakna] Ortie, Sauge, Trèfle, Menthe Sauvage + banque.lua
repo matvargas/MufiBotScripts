6fc316467106b7334cfe89aaa5d8a257479344a5
matvargas <m6.vargas@gmail.com>
2016-12-20 01:01:08 -0200

    Correção de Falha no banco

---
+++ Colheita/[Récolte][Alchimiste][Amakna] Ortie, Sauge, Trèfle, Menthe Sauvage + banque.lua
@@ -0,0 +1,204 @@
+﻿-- PODS --
+MAX_PODS = 95
+
+ELEMENTS_TO_GATHER = { 254, 255, 67, 66 ,311 }
+OPEN_BAGS = true
+
+DISPLAY_GATHER_COUNT = true
+
+function move ()
+  return
+{
+{ map = "99095051", changeMap = "410" },
+{ map = "5,21", gather = true, changeMap = "top" },
+{ map = "5,20", changeMap = "right" },
+	
+{ map = "6,20", gather = true, changeMap = "right" }, 
+	
+{ map = "7,20", gather = true, changeMap = "top" }, 
+	
+{ map = "7,19", changeMap = "left" }, 
+	
+{ map = "6,20", gather = true, changeMap = "top" }, 	
+{ map = "6,19", gather = true, changeMap = "left" }, 	
+{ map = "5,19", gather = true, changeMap = "bottom" }, 
+	
+{ map = "5,18", changeMap = "top" }, 
+	
+{ map = "5,17", changeMap = "top" }, 
+	
+{ map = "5,16", changeMap = "top" }, 	
+{ map = "5,15", changeMap = "top" }, 	
+{ map = "5,14", changeMap = "top" }, 
+	
+{ map = "5,13", changeMap = "top" }, 	
+{ map = "5,12", changeMap = "top" }, 
+{ map = "5,11", changeMap = "top" }, 
+{ map = "5,10", changeMap = "top" }, 
+{ map = "5,9", changeMap = "top" }, 
+{ map = "5,8", changeMap = "top" }, 
+{ map = "5,7", changeMap = "top" }, 
+{ map = "5,6", changeMap = "top" }, 	
+{ map = "5,5", changeMap = "top" }, 	
+{ map = "5,4", changeMap = "top" }, 	
+{ map = "5,3", changeMap = "top" }, 	
+{ map = "5,2", changeMap = "top" }, 	
+{ map = "5,1", changeMap = "top" }, 	
+{ map = "5,0", changeMap = "top" }, 	
+{ map = "5,-1", changeMap = "left" }, 
+{ map = "4,-1", changeMap = "left" }, 
+{ map = "3,-1", changeMap = "left" }, 
+{ map = "2,-1", changeMap = "bottom" },
+{ map = "2,-2", changeMap = "bottom" },
+	
+{ map = "2,-1", changeMap = "bottom"}, 	
+{ map = "2,0", changeMap = "bottom" }, 	
+{ map = "2,1", changeMap = "bottom" }, 	
+{ map = "2,2", changeMap = "bottom" }, 	
+{ map = "2,3", changeMap = "bottom" }, 
+{ map = "2,4", changeMap = "bottom" }, 	
+{ map = "2,5", changeMap = "bottom" }, 
+{ map = "2,6", changeMap = "bottom" }, 	
+{ map = "2,7", changeMap = "bottom" }, 	
+{ map = "2,8", changeMap = "bottom" }, 
+{ map = "2,9", changeMap = "bottom" }, 
+{ map = "2,10", changeMap = "bottom" }, 
+{ map = "2,11", changeMap = "bottom" }, 	
+{ map = "2,12", changeMap = "bottom" }, 	
+{ map = "2,13", changeMap = "bottom" }, 
+	
+{ map = "2,14", changeMap = "bottom" }, 
+{ map = "2,15", changeMap = "bottom" }, 	
+{ map = "2,16", changeMap = "bottom" },	
+{ map = "2,17", changeMap = "bottom" }, 	
+{ map = "2,18", changeMap = "bottom" }, 	
+{ map = "2,19", changeMap = "bottom" }, 	
+{ map = "2,20", changeMap = "bottom" }, 
+	
+{ map = "2,21", changeMap = "right" }, 
+{ map = "3,21", changeMap = "right" }, 
+{ map = "4,21", changeMap = "right" }, 
+}
+
+end
+
+function bank ()
+    return 
+{
+{ map = "5,21", changeMap = "left" },
+	
+{ map = "5,20", changeMap = "top" },
+	
+{ map = "6,20", changeMap = "left" },
+	
+{ map = "7,20", changeMap = "left" },
+	
+{ map = "7,19", changeMap = "left" },
+	
+{ map = "6,20", changeMap = "left" },
+	
+{ map = "6,19", changeMap = "left" },
+	
+{ map = "5,19", changeMap = "top" },
+	
+{ map = "5,18", changeMap = "top" },
+	
+{ map = "5,17", changeMap = "top" },
+	
+{ map = "5,16", changeMap = "top" },
+	
+{ map = "5,15", changeMap = "top" },
+	
+{ map = "5,14", changeMap = "top" },
+	
+{ map = "5,13", changeMap = "top" },
+	
+{ map = "5,12", changeMap = "top" },
+	
+{ map = "5,11", changeMap = "top" },
+	
+{ map = "5,10", changeMap = "top" },
+	
+{ map = "5,9", changeMap = "top" },
+	
+{ map = "5,8", changeMap = "top" },
+	
+{ map = "5,7", changeMap = "top" },
+	
+{ map = "5,6", changeMap = "top" },
+	
+{ map = "5,5", changeMap = "top" },
+	
+{ map = "5,4", changeMap = "top" },
+	
+{ map = "5,3", changeMap = "top" },
+	
+{ map = "5,2", changeMap = "top" },
+	
+{ map = "5,1", changeMap = "top" },
+	
+{ map = "5,0", changeMap = "top" },
+	
+{ map = "5,-1", changeMap = "left" },
+	
+{ map = "4,-1", changeMap = "left" },
+	
+{ map = "3,-1", changeMap = "left" },
+	
+{ map = "2,-1", changeMap = "top" },
+		
+{ map = "2,-1", changeMap = "top" },
+	
+{ map = "2,0", changeMap = "top" },
+	
+{ map = "2,1", changeMap = "top" },
+	
+{ map = "2,2", changeMap = "top" },
+	
+{ map = "2,3", changeMap = "top" },
+	
+{ map = "2,4", changeMap = "top" },
+	
+{ map = "2,5", changeMap = "top" },
+	
+{ map = "2,6", changeMap = "top" },
+	
+{ map = "2,7", changeMap = "top" },
+	
+{ map = "2,8", changeMap = "top" },
+	
+{ map = "2,9", changeMap = "top" },
+	
+{ map = "2,10", changeMap = "top" },
+	
+{ map = "2,11", changeMap = "top" },
+	
+{ map = "2,12", changeMap = "top" },
+	
+{ map = "2,13", changeMap = "top" },
+	
+{ map = "2,14", changeMap = "top" },
+	
+{ map = "2,15", changeMap = "top" },
+	
+{ map = "2,16", changeMap = "top" },
+	
+{ map = "2,17", changeMap = "top" },
+	
+{ map = "2,18", changeMap = "top" },
+	
+{ map = "2,19", changeMap = "top" },
+	
+{ map = "2,20", changeMap = "top" },
+	
+{ map = "2,21", changeMap = "left" },
+	
+{ map = "3,21", changeMap = "left" },
+	
+{ map = "4,21", changeMap = "left" },
+
+{ map = "88081177", door = "216" },        -- Map extérieure de la banque
+{ map = "99095051", npcBank = true },      -- Map intérieure de la banque
+}
+
+end
